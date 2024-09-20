47e4a7da377dec44d824cb6d5351ccd093c50095
Rafael C Santos <rafaelcsan705@gmail.com>
2024-09-20 15:33:15 +0100

    Initial Commit

---
+++ PopulationDataApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  PopulationDataApp
+//
+//  Created by Rafael Santos on 20/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Let's start the challenge.")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PopulationDataAppTests/PopulationDataAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PopulationDataAppTests.swift
+//  PopulationDataAppTests
+//
+//  Created by Rafael Santos on 20/09/2024.
+//
+
+import Testing
+@testable import PopulationDataApp
+
+struct PopulationDataAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
